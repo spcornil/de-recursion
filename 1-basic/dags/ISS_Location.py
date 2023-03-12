066f9548f154c7742fbd80a025c7abc488813655
spcornil <sean.cornillie@healthcatalyst.com>
2023-03-12 21:10:34 +0000

    completed ISS dag dev

--- 1-basic/dags/ISS_Location.py
+++ 1-basic/dags/ISS_Location.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 dag = DAG(
     dag_id="ISS_Location",
@@ -28,8 +30,16 @@ def _parse_data():
     df = (pd.DataFrame(df['iss_position'].values.tolist())
         .add_prefix('iss_')
         .join(df.drop('iss_position', axis=1)))
-    df = df[['timestamp','message','iss_longitude','iss_latitude']]
+    df = df.rename(columns={'timestamp':'date_time'})
+    df = df[['date_time','message','iss_longitude','iss_latitude']]
     df.to_csv('/tmp/iss_loc.csv', index=False, mode='w')
+
+def _store_location():
+    hook=PostgresHook(postgres_conn_id='postgres')
+    hook.copy_expert(
+        sql="COPY iss_location FROM stdin DELIMITER ',' CSV HEADER",
+        filename='/tmp/iss_loc.csv'
+    )
 
 download_location = BashOperator(
     task_id="download_location",
@@ -38,9 +48,29 @@ download_location = BashOperator(
 )
 
 parser_csv = PythonOperator(
-    task_id = "parser_csv",
+    task_id ='parser_csv',
     python_callable = _parse_data,
     dag=dag
 )
 
-download_location >> parser_csv
+create_table = PostgresOperator(
+    task_id='create_table',
+    postgres_conn_id='postgres',
+    sql='''
+        CREATE TABLE IF NOT EXISTS iss_location (
+            date_time TIMESTAMP NOT NULL,
+            message TEXT NOT NULL,
+            iss_longitude NUMERIC NOT NULL,
+            iss_latitude NUMERIC NOT NULL
+        );
+    '''
+)
+
+store_data = PythonOperator(
+    task_id='store_data',
+    python_callable=_store_location,
+    dag=dag,
+)
+
+
+create_table >> download_location >> parser_csv >> store_data
