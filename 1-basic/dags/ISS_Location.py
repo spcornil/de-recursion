7dfe211778c0f608fc4bfe460f44722e75a2fa5b
spcornil <sean.cornillie@healthcatalyst.com>
2023-03-12 19:37:56 +0000

    iss dag dev

--- 1-basic/dags/ISS_Location.py
+++ 1-basic/dags/ISS_Location.py
@@ -13,11 +13,34 @@ dag = DAG(
 
 dl_ts = datetime.now().strftime("%Y-%m-%d-%H%M%S")
 
-def iss_combine():
-
+def _parse_data():
+    import json
+    import pandas as pd
+    import glob
+    import os
+    files = glob.glob("/tmp/iss_*.json")
+    df = []
+    for f in files:
+        j = pd.read_json(f, lines=True)
+        df.append(j)
+    df = pd.concat(df)
+    df.reset_index(inplace=True, drop=True)
+    df = (pd.DataFrame(df['iss_position'].values.tolist())
+        .add_prefix('iss_')
+        .join(df.drop('iss_position', axis=1)))
+    df = df[['timestamp','message','iss_longitude','iss_latitude']]
+    df.to_csv('/tmp/iss_loc.csv', index=False, mode='w')
 
 download_location = BashOperator(
     task_id="download_location",
     bash_command=f"curl -o /tmp/iss_loc_{dl_ts}.json -L 'http://api.open-notify.org/iss-now.json'",
     dag=dag,
 )
+
+parser_csv = PythonOperator(
+    task_id = "parser_csv",
+    python_callable = _parse_data,
+    dag=dag
+)
+
+download_location >> parser_csv
